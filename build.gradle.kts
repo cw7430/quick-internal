93b9de684b4313d8cdeee33a7919f7b9c818e0db
cw7430 <cw282@naver.com>
2026-09-12 11:30:37 +0900

    데이터 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.quick"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0"
 
 java {
 	toolchain {
---
+++ src/main/resources/db/migration/dev/R__local_test_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users
+    (nick_name, gender, role)
+VALUES ('관리자', 'M', 'ADMIN');
+
+INSERT INTO native_users
+    (id, email, password_hash)
+VALUES (1, 'email@email.com', '$2a$10$o2t3/O064cL1Bz/D3diOI.6MRJCXHmRlEVto1ahJwNeubrD1Jt7ki');
