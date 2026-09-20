64b425a49d620ac0af65bbc0851837fb8b168457
cw7430 <cw282@naver.com>
2026-09-20 12:56:41 +0900

    채팅방 방 대량 생성

--- src/main/resources/db/migration/dev/R__local_test_data.sql
+++ src/main/resources/db/migration/dev/R__local_test_data.sql
@@ -37,7 +37,7 @@ VALUES (1, '관리자', 'M', 'ADMIN'),
        (3, '테스트2', 'F', 'USER'),
        (4, '테스트3', 'F', 'USER');
 
--- 2. users 5~50000번 대량 생성
+-- 2. users 5~50,000번 대량 생성
 INSERT INTO users (id, nick_name, gender, role)
 WITH digits (d) AS (SELECT 0
                     UNION ALL
@@ -59,11 +59,11 @@ WITH digits (d) AS (SELECT 0
                     UNION ALL
                     SELECT 9),
      seq (n) AS (SELECT d4.d * 10000 + d3.d * 1000 + d2.d * 100 + d1.d * 10 + d0.d
-                 FROM digits d0,
-                      digits d1,
-                      digits d2,
-                      digits d3,
-                      digits d4)
+                 FROM digits d0
+                          CROSS JOIN digits d1
+                          CROSS JOIN digits d2
+                          CROSS JOIN digits d3
+                          CROSS JOIN digits d4)
 SELECT n,
        CONCAT('테스트', n - 1),
        IF(n % 2 = 0, 'M', 'F'),
@@ -83,11 +83,59 @@ VALUES (1, 'admin', '$2a$10$3I4CT9WUIy..IHBvJkbDT.IVaXSP7CjLfUgNB3EZtitup8BgSv6Y
        (3, 'email2@email.com', '$2a$10$3I4CT9WUIy..IHBvJkbDT.IVaXSP7CjLfUgNB3EZtitup8BgSv6Y2'),
        (4, 'email3@email.com', '$2a$12$Jyz/IhFJrfOmvNdekDXALup3fvvqiYvle64CIBDVY560bFT1sBaDG');
 
--- 5. native_users 5~50000번 대량 생성
+-- 5. native_users 5~50,000번 대량 생성
 INSERT INTO native_users (id, email, password_hash)
 SELECT id,
        CONCAT('email', id - 1, '@email.com'),
        '$2a$10$3I4CT9WUIy..IHBvJkbDT.IVaXSP7CjLfUgNB3EZtitup8BgSv6Y2'
 FROM users
-WHERE id BETWEEN 5 AND 50000
+WHERE id BETWEEN 5 AND (SELECT id FROM users ORDER BY id DESC LIMIT 1)
 ORDER BY id;
+
+-- 6. chat_room 대량 생성
+INSERT INTO chat_room (valid)
+WITH digits (d) AS (SELECT 0
+                    UNION ALL
+                    SELECT 1
+                    UNION ALL
+                    SELECT 2
+                    UNION ALL
+                    SELECT 3
+                    UNION ALL
+                    SELECT 4
+                    UNION ALL
+                    SELECT 5
+                    UNION ALL
+                    SELECT 6
+                    UNION ALL
+                    SELECT 7
+                    UNION ALL
+                    SELECT 8
+                    UNION ALL
+                    SELECT 9),
+     seq (n) AS (SELECT d4.d * 10000 + d3.d * 1000 + d2.d * 100 + d1.d * 10 + d0.d
+                 FROM digits d0
+                          CROSS JOIN digits d1
+                          CROSS JOIN digits d2
+                          CROSS JOIN digits d3
+                          CROSS JOIN digits d4)
+SELECT 'Y'
+FROM seq
+WHERE n BETWEEN 5 AND ((SELECT id FROM users ORDER BY id DESC LIMIT 1) / 2)
+ORDER BY n;
+
+-- 6. chat_room 대량 생성
+INSERT INTO chat_member (chat_room_id, user_id, accepted)
+WITH user_info AS (SELECT MAX(id) AS max_id, (MAX(id) - 4) AS user_count
+                   FROM users),
+     ranked_room AS (SELECT id, ROW_NUMBER() OVER (ORDER BY id) AS rn
+                     FROM chat_room)
+SELECT cr.id                                                                       AS chat_room_id,
+       5 + CAST(MOD(((cr.rn - 1) * 2 + m.member_offset), ui.user_count) AS SIGNED) AS user_id,
+       'Y'
+FROM ranked_room cr
+         CROSS JOIN user_info ui
+         CROSS JOIN (SELECT 0 AS member_offset
+                     UNION ALL
+                     SELECT 1) m
+ORDER BY cr.id, member_offset;
